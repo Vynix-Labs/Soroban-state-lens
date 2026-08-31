0c3cd0d78f680434dbd58e65b485ec4903a3ca71
Ikemhood <91676649+ikemHood@users.noreply.github.com>
2026-08-31 08:40:02 +0100

    Remove unused snapshot test import

--- src/test/components/sidebarSnapshotControls.test.tsx
+++ src/test/components/sidebarSnapshotControls.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { beforeEach, describe, expect, it } from 'vitest'
 
 import Sidebar from '../../components/global/Sidebar'
